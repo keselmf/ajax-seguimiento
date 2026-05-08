1dc9a83a3b372ba5cea3c8fa0354f2cd25680a56
keselmf <keselmf@gmail.com>
2026-05-07 21:12:27 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,25 @@
-const CACHE_NAME = 'ajax-seguimiento-v1';
-const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+var GHPATH = '/ajax-seguimiento';
+var CACHE = 'ajax-v1';
+var URLS = [
+  GHPATH + '/',
+  GHPATH + '/index.html',
+  GHPATH + '/manifest.json',
+  GHPATH + '/icon-192.png',
+  GHPATH + '/icon-512.png'
+];
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(ASSETS); }));
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(URLS); }));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', function(e) {
-  e.waitUntil(caches.keys().then(function(keys) {
-    return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+self.addEventListener('activate', function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){ return k!==CACHE; }).map(function(k){ return caches.delete(k); }));
   }));
   self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(fetch(e.request).catch(function() { return caches.match(e.request); }));
+self.addEventListener('fetch', function(e){
+  e.respondWith(fetch(e.request).catch(function(){ return caches.match(e.request); }));
 });
